516460cfe815178636e162e0e7f3667a8b184439
chimaobisvgj <igwilochimaobi139@gmail.com>
2025-09-30 10:48:52 +0200

    first test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Create one property
+        RealEstate house = new RealEstate("Budapest", 1000, 100, 4, RealEstate.Genre.FAMILYHOUSE);
+
+        // Print the property
+        System.out.println(house);
+
+        // Print the total price
+        System.out.println("Total price: " + house.getTotalPrice());
+
+        // Print average sqm per room
+        System.out.println("Average sqm per room: " + house.averageSqmPerRoom());
+
+        // Apply a 10% discount and print again
+        house.makeDiscount(10);
+        System.out.println("After 10% discount:");
+        System.out.println(house);
     }
 }
